4f8a1dc126bd0ca3bcb8b51f106c3d4bcaa1697e
jherson <rodriguezpaul98@gmail.com>
2025-06-06 17:39:49 -0400

    prisma configuration

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { PrismaClient } from 'generated/prisma';
+
 
 @Injectable()
-export class OrdersService {
+export class OrdersService extends PrismaClient implements OnModuleInit{
+  private readonly logger = new Logger('OrdersService');
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Database connection established');
+  }
+
   create(createOrderDto: CreateOrderDto) {
     return 'This action adds a new order';
   }
